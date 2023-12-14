c59c19a761f3dfea9bbf1e00e155b97ff850d563
Apple <jonathanhyefur@gmail.com>
2023-12-14 06:33:57 +0100

    push notification added

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const expressWs = require('express-ws');
 const app = express();
+const OneSignal = require('@onesignal/node-onesignal');
+
+
+const ONESIGNAL_APP_ID = "f8750b39-c115-4984-b8fa-b06a941503c8";
+
+const app_key_provider = {
+    getToken() {
+        return 'NWEwY2Q0YTctNDA0YS00YjZmLWFhMWYtMjE2N2FmOTgxZGQw';
+    }
+};
+
+const configuration = OneSignal.createConfiguration({
+    authMethods: {
+        app_key: {
+        	tokenProvider: app_key_provider
+        }
+    }
+});
+const client = new OneSignal.DefaultApi(configuration);
 
 let lightSwitch;
 let doorMode;
@@ -14,37 +33,6 @@ const espClient = [];
 
 let access = 0;
 
-app.post('/',(req, res)=>{
-  const {username, password} = req.body;
-  if(username == "Hyefur" && password == "password"){
-    access = 1;
-    res.status(200).json({
-      message: 'Access Granted',
-    });
-  }else{
-    access = 0;
-    res.status(200).json({
-      message: 'Access Denied',
-    });
-  }
-});
-
-app.get('/esp',(req, res)=>{
-  // const {username, password} = req.body;
-  // if(username == "Hyefur" && password == "password"){
-  //   access = 1;
-  //   res.status(200).json({
-  //     message: 'Access Granted',
-  //   });
-  // }else{
-    access = 0;
-    res.status(200).json({
-      message: 'Access Denied',
-    });
-  //}
-});
-
-
 app.get('/logout',(req, res)=>{
     access = 0;
     res.status(200).json({
@@ -57,25 +45,47 @@ app.ws('/esp', (ws) => {
 console.log('ESP32 connected');
 espClient.push(ws);
 
-espClient.forEach((espSocket) => {
-  espSocket.send(`Connected`)
-    });
+  wsClients.forEach((flutterSocket) => {
+    flutterSocket.send(`Smart Home Connected`)
+  });   
                
-ws.on('message', (message) => {
- console.log(`Received from ESP32: ${message}`);
-   wsClients.forEach((flutterSocket) => {
-   flutterSocket.send(message);
+  ws.on('message', (message) => {
+    console.log(`Received from ESP32: ${message}`);
+    wsClients.forEach((flutterSocket) => {
+    flutterSocket.send(message);
      });
    });
+
+   ws.on('close', () => {
+    console.log('ESP disconnected'); 
+    espClient.splice(espClient.indexOf(ws), 1);
+
+    wsClients.forEach((flutterSocket) => {
+      flutterSocket.send(`Smart Home Disconnected`)
+    }); 
+
+  });
+
 });
 
-app.ws('/flutter', (ws) => {
+app.ws('/flutter', async(ws) => {
   console.log('Flutter app connected');
   wsClients.push(ws);
 
   wsClients.forEach((flutterSocket) => {
     flutterSocket.send(`Connected`)
       });
+
+      const notification = new OneSignal.Notification();
+        notification.app_id = ONESIGNAL_APP_ID;
+        notification.included_segments = ['Subscribed Users'];
+        notification.headings = {
+            en: "Smart Home"
+          }
+        notification.contents = {
+            en: `User connected`
+        };
+        const {id} = await client.createNotification(notification);
 
   ws.on('message', (newMessage) => {
 
